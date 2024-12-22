8348fdbdfa7f8271f7b13a43872323c8e4e0c8c0
Budi Yuwono <budi@bagasi.id>
2024-12-22 16:19:57 +1100

    fix: simplify Google Analytics implementation without TypeScript types

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,44 +1,43 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const pageview = (url: string) => {
-  if (window.gtag) {
-    window.gtag('config', 'G-Z60BC1QXD9', {
-      page_path: url,
-    });
-  }
-};
+const TRACKING_ID = 'G-Z60BC1QXD9';
 
 const GoogleAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    // Load GA script
     const script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-Z60BC1QXD9';
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${TRACKING_ID}`;
     document.head.appendChild(script);
 
-    // Initialize GA
-    window.dataLayer = window.dataLayer || [];
-    function gtag(...args: any[]) {
-      window.dataLayer.push(arguments);
-    }
-    window.gtag = gtag;
-    gtag('js', new Date());
-    gtag('config', 'G-Z60BC1QXD9', {
-      send_page_view: true,
-      cookie_flags: 'SameSite=None;Secure'
-    });
+    const scriptContent = document.createElement('script');
+    scriptContent.textContent = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${TRACKING_ID}', {
+        page_location: window.location.href,
+        page_path: window.location.pathname + window.location.search,
+        cookie_flags: 'SameSite=None;Secure'
+      });
+    `;
+    document.head.appendChild(scriptContent);
 
     return () => {
       document.head.removeChild(script);
+      document.head.removeChild(scriptContent);
     };
   }, []);
 
   useEffect(() => {
-    const path = location.pathname + location.search;
-    pageview(path);
+    if (window.gtag) {
+      window.gtag('config', TRACKING_ID, {
+        page_location: window.location.href,
+        page_path: location.pathname + location.search,
+      });
+    }
   }, [location]);
 
   return null;
