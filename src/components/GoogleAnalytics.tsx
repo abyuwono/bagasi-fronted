ef019b62508c66584e4ff7bca99d16361da352a8
Budi Yuwono <budi@bagasi.id>
2024-12-23 16:01:06 +1100

    feat: switch to react-ga4 for Google Analytics tracking

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga4';
 
 const TRACKING_ID = 'G-Z60BC1QXD9';
 
@@ -7,14 +8,15 @@ const GoogleAnalytics = () => {
   const location = useLocation();
 
   useEffect(() => {
-    if (typeof window.gtag !== 'undefined') {
-      window.gtag('event', 'page_view', {
-        page_title: document.title,
-        page_location: window.location.href,
-        page_path: location.pathname + location.search,
-        send_to: TRACKING_ID
-      });
-    }
+    ReactGA.initialize(TRACKING_ID);
+  }, []);
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: 'pageview',
+      page: location.pathname + location.search,
+      title: document.title
+    });
   }, [location]);
 
   return null;
