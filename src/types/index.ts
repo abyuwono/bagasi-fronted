eaf47a35d27b4a08f5f0dcef0d7b03e29773f0d1
Budi Yuwono <budi@bagasi.id>
2025-01-03 18:01:04 +1100

    feat: add merchantName to ShopperAd type

--- src/types/index.ts
+++ src/types/index.ts
@@ -78,6 +78,7 @@ export interface ShopperAd {
     customDisplayName?: string;
   };
   productUrl: string;
+  merchantName?: string;
   productName: string;
   productImage: string;
   cloudflareImageId?: string;
